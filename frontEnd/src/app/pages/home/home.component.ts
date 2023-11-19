6eef279d1231abd692b592658eb0b148bb8faa5f
Komal Bandi <komal@komalbandi.com>
2023-11-19 18:42:27 +1000

    Feat: Added accordion to display notifications

--- frontEnd/src/app/pages/home/home.component.ts
+++ frontEnd/src/app/pages/home/home.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { ServerService } from './common/server.service';
+import { Notifications as NotificationsInterface } from '../../interfaces/notifications';
+import { first } from 'rxjs';
+import { Notifications as NotificationCollection } from '../../models/collection/notifications';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  notificationDatas?: NotificationCollection;
+  constructor(public serverService: ServerService) {
+    this.getNotifications();
+  }
 
+  getNotifications() {
+    this.serverService
+      .getNotification<NotificationsInterface[]>()
+      .pipe(first())
+      .subscribe({
+        next: (datas: NotificationsInterface[]) => {
+          this.notificationDatas = new NotificationCollection(datas);
+          console.log(this.notificationDatas.all[0].header);
+        }
+      });
+  }
 }
