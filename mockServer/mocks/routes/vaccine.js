d5aed07e48d2c204cd5a924258a8bccb81e3211e
Komal Bandi <komal@komalbandi.com>
2023-11-19 19:56:56 +1000

    Feat: Backend, added end point to retrieve vaccination details

---
+++ mockServer/mocks/routes/vaccine.js
@@ -0,0 +1,37 @@
+// Use this file only as a guide for first steps using routes. Delete it when you have added your own route files.
+// For a detailed explanation regarding each routes property, visit:
+// https://mocks-server.org/docs/usage/routes
+
+// users data
+const NOTIFICATIONS = [
+  {
+    id: 1,
+    header: "Covid vaccine",
+    details:
+      "For covid, active for two years, no known adverse interactions with diabetes, Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+  },
+  {
+    id: 2,
+    header: "Other covid vaccine",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.",
+  },
+];
+
+module.exports = [
+  {
+    id: "get-vaccination", // route id
+    url: "/api/vaccination", // url in express format
+    method: ["POST"], // HTTP method
+    variants: [
+      {
+        id: "success", // variant id
+        type: "json", // variant handler id
+        options: {
+          status: 200, // status to send
+          body: NOTIFICATIONS, // body to send
+        },
+      },
+    ],
+  },
+];
