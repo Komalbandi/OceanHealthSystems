e45ce8f8577977a81ecb7a19458a1b4907bf90e2
Komal Bandi <komal@komalbandi.com>
2023-11-19 20:56:40 +1000

    Feat: created form for vaccination search

--- frontEnd/src/app/pages/staff/staff.component.ts
+++ frontEnd/src/app/pages/staff/staff.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { ServerService } from './common/server.service';
+import { first, pipe } from 'rxjs';
 
 @Component({
   selector: 'app-staff',
   templateUrl: './staff.component.html',
-  styleUrls: ['./staff.component.scss']
+  styleUrls: ['./staff.component.scss'],
 })
 export class StaffComponent {
+  searchVaccineForm: FormGroup;
+  constructor(public fb: FormBuilder, public serverService: ServerService) {
+    this.searchVaccineForm = fb.group({
+      vaccineSearchText: new FormControl('', [Validators.required]),
+    });
+  }
 
+  searchVaccine() {
+    if (this.searchVaccineForm.status !== 'INVALID') {
+      console.log(this.searchVaccineForm.value);
+      this.getVaccine(this.searchVaccineForm.value);
+    }
+  }
+
+  getVaccine(data: any) {
+    this.serverService
+      .getVaccine(data)
+      .pipe(first())
+      .subscribe({
+        next: () => {},
+        error: () => {},
+      });
+  }
 }
--- frontEnd/src/app/pages/staff/staff.module.ts
+++ frontEnd/src/app/pages/staff/staff.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from '@angular/common';
 import { StaffComponent } from './staff.component';
 import { StaffRoutingModule } from './staff-routing.module';
 import { HomeTemplateModule } from '../../library/template/home-template/home-template.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [StaffComponent],
-  imports: [CommonModule, StaffRoutingModule, HomeTemplateModule],
+  imports: [
+    CommonModule,
+    StaffRoutingModule,
+    HomeTemplateModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
 })
 export class StaffModule {}
