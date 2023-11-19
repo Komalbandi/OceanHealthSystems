4ed551362c5a5f0ee6789b59a3fb0b864510f403
Komal Bandi <komal@komalbandi.com>
2023-11-20 09:57:38 +1000

    Feat: menu navigation fix

--- frontEnd/src/app/library/template/home-template/home-template.component.ts
+++ frontEnd/src/app/library/template/home-template/home-template.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-template',
   templateUrl: './home-template.component.html',
-  styleUrls: ['./home-template.component.scss']
+  styleUrls: ['./home-template.component.scss'],
 })
 export class HomeTemplateComponent {
-
+  constructor(public router: Router) {
+    console.log(router.url);
+  }
 }
--- frontEnd/src/app/library/template/home-template/home-template.module.ts
+++ frontEnd/src/app/library/template/home-template/home-template.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeTemplateComponent } from './home-template.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [HomeTemplateComponent],
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   exports: [HomeTemplateComponent],
 })
 export class HomeTemplateModule {}
